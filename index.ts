00aa6618294743ee38d8a403179af0b4fb0b19ee
David <dav2070@web.de>
2021-09-29 20:07:34 +0200

    Implement Toggle component
    - Move the toggle component file to separate folder
    - Create style file for the styles of the toggle component
    - Implement the toggle component with checked property and change event

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-export { Toggle } from './src/toggle'
+import { Toggle, toggleTagName } from './src/toggle/toggle'
+
+declare global {
+	interface HTMLElementTagNameMap {
+		[toggleTagName]: Toggle,
+	}
+}
+
+export { Toggle }
---
+++ src/toggle/toggle.ts
@@ -0,0 +1,45 @@
+import { LitElement, html } from 'lit'
+import { customElement, property, state } from 'lit/decorators.js'
+import { classMap } from 'lit/directives/class-map.js'
+import { toggleStyles } from './toggle.styles'
+
+export const toggleTagName = "dav-toggle"
+
+@customElement(toggleTagName)
+export class Toggle extends LitElement {
+	static styles = [toggleStyles]
+
+	@state()
+	inputClasses = {
+		checked: false
+	}
+
+	@property({ type: Boolean })
+	checked: boolean = false
+
+	private checkboxClicked(event: PointerEvent) {
+		event.preventDefault()
+
+		this.dispatchEvent(new CustomEvent("change", {
+			detail: {
+				checked: this.checked
+			}
+		}))
+	}
+
+	render() {
+		// Update the UI based on the properties
+		this.inputClasses.checked = this.checked
+
+		return html`
+			<label class="switch">
+				<input
+					class=${classMap(this.inputClasses)}
+					type="checkbox"
+					?checked=${this.checked}
+					@click=${this.checkboxClicked}>
+				<span class="slider"></span>
+			</label>
+		`
+	}
+}
