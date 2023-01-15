2645edcfbd13d76cb40683d6b3f9d7917513ff84
David <zurek.dav@gmail.com>
2023-01-15 18:30:56 +0100

    Add basic doc file for IconButton component

---
+++ src/icon-button/icon-button.doc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+	name: 'IconButton',
+	examples: [
+		{
+			title: 'Default Button',
+         controller: function (element) {
+            
+			},
+			template: `
+				<dav-icon-button>
+					<i class="fa-solid fa-magnifying-glass"></i>
+				</dav-icon-button>
+			`
+		}
+	]
+}
