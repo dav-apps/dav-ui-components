7bddb77fc346a08d72fe1af6e53c7cf52a7542ed
David <zurek.dav@gmail.com>
2023-02-01 21:38:42 +0100

    Update button doc file with props and examples for colors and tonal

--- src/button/button.doc.cjs
+++ src/button/button.doc.cjs
@@ -1,12 +1,33 @@
 module.exports = {
 	name: 'Button',
+	properties: [
+		{
+			name: "color",
+			type: "String",
+			values: `"primary", "secondary"`
+		},
+		{
+			name: "tonal",
+			type: "Boolean",
+			values: "true / false"
+		}
+	],
 	examples: [
 		{
-			title: 'Default Button',
-			controller: function (element) {
-				element.onclick = () => alert('Hello!');
-			},
-			template: `<dav-button>Greet</dav-button>`
+			title: 'Primary button',
+			template: `<dav-button>Hello World</dav-button>`
+		},
+		{
+			title: "Secondary button",
+			template: `<dav-button color="secondary">Hello World</dav-button>`
+		},
+		{
+			title: "Primary tonal button",
+			template: `<dav-button tonal>Hello World</dav-button>`
+		},
+		{
+			title: "Secondary tonal button",
+			template: `<dav-button color="secondary" tonal>Hello World</dav-button>`
 		}
 	]
 }
