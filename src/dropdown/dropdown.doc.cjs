21713652efc0ed8a7712d3b71b58ae1b25ee534e
David <zurek.dav@gmail.com>
2023-02-04 23:16:08 +0100

    Add doc file for Dropdown component

---
+++ src/dropdown/dropdown.doc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+	name: "Dropdown",
+	examples: [
+		{
+			title: "Default Dropdown",
+			controller: function (element) {
+				element.style.height = "200px"
+				let dropdown = element.getElementsByTagName("dav-dropdown")[0]
+
+				dropdown.options = [
+					{
+						key: 'system',
+						value: 'System default',
+						type: 0
+					},
+					{
+						key: 'light',
+						value: 'Light',
+						type: 0
+					},
+					{
+						key: 'dark',
+						value: 'Dark',
+						type: 0
+					}
+				]
+			},
+			template: `
+				<dav-dropdown
+					label="Theme">
+				</dav-dropdown>
+			`
+		}
+	]
+}
