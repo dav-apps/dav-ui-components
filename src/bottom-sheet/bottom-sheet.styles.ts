b266a163b5cba3cfc5253cf02ce473d09d71487d
David <zurek.dav@gmail.com>
2023-02-06 22:05:08 +0100

    Implement basic BottomSheet with functionality for showing and hiding

--- src/bottom-sheet/bottom-sheet.styles.ts
+++ src/bottom-sheet/bottom-sheet.styles.ts
@@ -2,12 +2,17 @@ import { css } from "@lit/reactive-element"
 
 export const bottomSheetStyles = css`
 	#container {
+		display: none;
 		position: fixed;
 		top: 0;
 		bottom: 0;
 		left: 0;
 		right: 0;
 		z-index: 100;
+	}
+
+	#container.visible {
+		display: block;
 	}
 
 	#overlay {
@@ -17,5 +22,32 @@ export const bottomSheetStyles = css`
 		left: 0;
 		right: 0;
 		background-color: var(--md-sys-color-scrim-light);
+	}
+
+	#content-container {
+		position: fixed;
+		bottom: 0;
+		width: 100%;
+		display: grid;
+		grid-template-columns: auto max-content auto;
+		background-color: transparent;
+	}
+
+	#content-left-overlay {
+		grid-column: 1;
+	}
+
+	#content-right-overlay {
+		grid-column: 3;
+	}
+
+	#content {
+		grid-column: 2;
+		justify-self: stretch;
+		min-width: 300px;
+		max-width: 600px;
+		height: 200px;
+		border-radius: 16px 16px 0 0;
+		background-color: white;
 	}
 `
--- src/bottom-sheet/bottom-sheet.ts
+++ src/bottom-sheet/bottom-sheet.ts
@@ -1,5 +1,6 @@
 import { LitElement, html } from 'lit'
 import { customElement, property, state } from 'lit/decorators.js'
+import { classMap } from 'lit/directives/class-map.js'
 import { globalStyles } from '../styles.js'
 import { bottomSheetStyles } from './bottom-sheet.styles.js'
 
@@ -9,16 +10,40 @@ export const bottomSheetTagName = "dav-bottom-sheet"
 export class BottomSheet extends LitElement {
 	static styles = [globalStyles, bottomSheetStyles]
 
+	@state() private containerClasses = {
+		visible: false
+	}
+
+	@property({ type: Boolean }) visible: boolean = false
+
 	private overlayClick() {
 		this.dispatchEvent(new CustomEvent("dismiss"))
 	}
 
 	render() {
+		this.containerClasses.visible = this.visible
+
 		return html`
-			<div id="container">
+			<div id="container" class=${classMap(this.containerClasses)}>
 				<div
 					id="overlay"
 					@click=${this.overlayClick}>
+				</div>
+
+				<div id="content-container">
+					<div
+						id="content-left-overlay"
+						@click=${this.overlayClick}>
+					</div>
+
+					<div id="content">
+						<slot></slot>
+					</div>
+
+					<div
+						id="content-right-overlay"
+						@click=${this.overlayClick}>
+					</div>
 				</div>
 			</div>
 		`
