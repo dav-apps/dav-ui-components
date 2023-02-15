e82a0e8cbd92181503573bf0f51e19ec24a9bef6
David <zurek.dav@gmail.com>
2023-02-15 22:34:47 +0100

    Add doc file for ProgressRing component

---
+++ src/progress-ring/progress-ring.doc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+	name: 'ProgressRing',
+	properties: [
+		{
+			name: "size",
+			type: "Number"
+		},
+		{
+			name: "color",
+			type: "String"
+		}
+	],
+	examples: [
+		{
+			title: "Default ProgressRing",
+			template: `
+				<dav-progress-ring size="42">
+				</dav-progress-ring>
+			`
+		}
+	]
+}
