5cc65f24793787bd05266d204f890fdf7d2ef2f7
David <zurek.dav@gmail.com>
2023-02-26 01:08:49 +0100

    Update header doc file for using slot instead of header attribute

--- src/header/header.doc.cjs
+++ src/header/header.doc.cjs
@@ -5,32 +5,35 @@ module.exports = {
 			title: "Normal Header",
 			template: `
 				<dav-header
-					header="Hello World"
 					backButtonVisible
 					editButtonVisible
-				></dav-header>
+				>
+					Hello World
+				</dav-header>
 			`
 		},
 		{
 			title: "Small Header",
 			template: `
 				<dav-header
-					header="Hello World"
 					size="small"
 					backButtonVisible
 					editButtonVisible
-				></dav-header>
+				>
+					Hello World
+				</dav-header>
 			`
 		},
 		{
 			title: "Large Header",
 			template: `
 				<dav-header
-					header="Hello World"
 					size="large"
 					backButtonVisible
 					editButtonVisible
-				></dav-header>
+				>
+					Hello World
+				</dav-header>
 			`
 		}
 	]
