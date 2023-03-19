e86f2e602a4b473781a176f25e8a71907f10a4f8
David <zurek.dav@gmail.com>
2023-03-19 20:47:07 +0100

    Add card doc file

---
+++ src/card/card.doc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "Card",
+	examples: [
+		{
+			title: "Card with headline and subhead",
+			template: `
+				<dav-card
+					headline="A Daughter of the Samurai"
+					subhead="by Etsu Inagaki Sugimoto">
+				</dav-card>
+			`
+		}
+	]
+}
