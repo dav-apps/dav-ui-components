268e7964c088e3aea31777c07b45d748cd5b0be0
David <zurek.dav@gmail.com>
2023-03-27 22:15:46 +0200

    Implement ListItem component layout & styles
    - Add imageSrc, headline and subhead properties

--- src/list-item/list-item.styles.ts
+++ src/list-item/list-item.styles.ts
@@ -1,3 +1,37 @@
 import { css } from "@lit/reactive-element"
 
-export const listItemStyles = css``
+export const listItemStyles = css`
+	.list-item-container {
+		display: flex;
+		align-items: center;
+		padding: 8px 16px;
+		background-color: var(--md-sys-color-surface-light);
+	}
+
+	.list-item-image-container {
+		height: 56;
+		width: 56;
+		margin-right: 16px;
+	}
+
+	.list-item-image {
+		border-radius: 6px;
+	}
+
+	.list-item-body {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.list-item-headline {
+		color: var(--md-sys-color-on-surface-light);
+		font-size: 16px;
+		margin: 0;
+	}
+
+	.list-item-subhead {
+		color: var(--md-sys-color-on-surface-variant-light);
+		font-size: 14px;
+		margin: 0;
+	}
+`
--- src/list-item/list-item.ts
+++ src/list-item/list-item.ts
@@ -9,9 +9,49 @@ export const listItemTagName = "dav-list-item"
 export class ListItem extends LitElement {
 	static styles = [globalStyles, listItemStyles]
 
+	@property({ type: String }) imageSrc: string = ""
+	@property({ type: String }) headline: string = ""
+	@property({ type: String }) subhead: string = ""
+
+	getImage() {
+		if (this.imageSrc.length > 0) {
+			return html`
+				<div class="list-item-image-container">
+					<dav-blurhash-image
+						class="list-item-image"
+						src=${this.imageSrc}
+						height="56"
+					></dav-blurhash-image>
+				</div>
+			`
+		}
+	}
+
+	getHeadline() {
+		if (this.headline.length > 0) {
+			return html`
+				<p class="list-item-headline">${this.headline}</p>
+			`
+		}
+	}
+
+	getSubhead() {
+		if (this.subhead.length > 0) {
+			return html`
+				<p class="list-item-subhead">${this.subhead}</p>
+			`
+		}
+	}
+
 	render() {
 		return html`
-			<h1>Hello World</h1>
+			<div class="list-item-container">
+				${this.getImage()}
+
+				<div class="list-item-body">
+					${this.getHeadline()} ${this.getSubhead()}
+				</div>
+			</div>
 		`
 	}
 }
