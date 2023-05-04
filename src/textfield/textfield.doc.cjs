32ad32235f304af927ca1dad43b75c7847721ca9
David <zurek.dav@gmail.com>
2023-05-05 00:10:59 +0200

    Add disabled & numeric textfield examples to doc file

--- src/textfield/textfield.doc.cjs
+++ src/textfield/textfield.doc.cjs
@@ -31,6 +31,18 @@ module.exports = {
 			`
 		},
 		{
+			title: "Disabled Textfield",
+			template: `
+				<dav-textfield
+					value="Monty"
+					label="Name"
+					placeholder="Your name"
+					autocomplete="given-name"
+					disabled
+				></dav-textfield>
+			`
+		},
+		{
 			title: "Textfield with error",
 			template: `
 				<dav-textfield
@@ -39,6 +51,19 @@ module.exports = {
 					placeholder="Your name"
 					autocomplete="given-name"
 					errorMessage="Please enter your real name"
+				></dav-textfield>
+			`
+		},
+		{
+			title: "Textfield for numeric values only",
+			template: `
+				<dav-textfield
+					value="20"
+					label="Age"
+					placeholder="Your age"
+					type="number"
+					min="10"
+					max="100"
 				></dav-textfield>
 			`
 		}
