2d295b36a2bb3f81b4b2e0ff339bd496d09bf0cf
David <zurek.dav@gmail.com>
2023-08-27 15:25:52 +0200

    Add separate example for non-dismissable BottomSheet
    - Update example controllers to use element variable to retrieve elements

--- src/bottom-sheet/bottom-sheet.doc.cjs
+++ src/bottom-sheet/bottom-sheet.doc.cjs
@@ -3,7 +3,7 @@ module.exports = {
 	examples: [
 		{
 			title: "Default BottomSheet",
-			controller: function(element) {
+			controller: function (element) {
 				const touchStart = "touchstart"
 				const touchMove = "touchmove"
 				const touchEnd = "touchend"
@@ -13,21 +13,83 @@ module.exports = {
 				let swipeStart = false
 				let startPosition = 0
 
-				let openButton = document.getElementById("open-button")
-				let openNonDismissableButton = document.getElementById(
-					"open-non-dismissable-button"
-				)
-				let closeButton = document.getElementById("close-button")
-				let bottomSheet = document.getElementsByTagName(
-					"dav-bottom-sheet"
-				)[0]
+				let openButton = element.getElementsByTagName("dav-button")[0]
+				let closeButton = element.getElementsByTagName("dav-button")[1]
+				let bottomSheet =
+					element.getElementsByTagName("dav-bottom-sheet")[0]
 
 				openButton.onclick = () => {
-					bottomSheet.dismissable = true
 					bottomSheet.visible = true
 				}
 
-				openNonDismissableButton.onclick = () => {
+				closeButton.onclick = () => {
+					bottomSheet.visible = false
+				}
+
+				bottomSheet.addEventListener("dismiss", () => {
+					bottomSheet.visible = false
+				})
+
+				bottomSheet.addEventListener(touchStart, handleTouch)
+				bottomSheet.addEventListener(touchMove, handleTouch)
+				bottomSheet.addEventListener(touchEnd, handleTouch)
+
+				function handleTouch(event) {
+					if (event.touches.length > 1) return
+
+					if (event.type == touchStart) {
+						touchStartY = event.touches.item(0).screenY
+						swipeStart = true
+						bottomSheet.animatePosition = false
+					} else if (event.type == touchMove) {
+						touchDiffY = touchStartY - event.touches.item(0).screenY
+
+						if (swipeStart) {
+							startPosition = bottomSheet.position
+							swipeStart = false
+						}
+
+						bottomSheet.position = touchDiffY + startPosition
+					} else if (event.type == touchEnd) {
+						touchStartY = 0
+						touchDiffY = 0
+						startPosition = 0
+						bottomSheet.animatePosition = true
+					}
+				}
+			},
+			template: `
+				<dav-button>
+					Open bottom sheet
+				</dav-button>
+
+				<dav-bottom-sheet position="100">
+					<h1>Hello World</h1>
+					<dav-button
+						style="margin-bottom: 24px"
+						size="small"
+					>Hide</dav-button>
+				</dav-bottom-sheet>
+			`
+		},
+		{
+			title: "Non-dismissable BottomSheet",
+			controller: function (element) {
+				const touchStart = "touchstart"
+				const touchMove = "touchmove"
+				const touchEnd = "touchend"
+
+				let touchStartY = 0
+				let touchDiffY = 0
+				let swipeStart = false
+				let startPosition = 0
+
+				let openButton = element.getElementsByTagName("dav-button")[0]
+				let closeButton = element.getElementsByTagName("dav-button")[1]
+				let bottomSheet =
+					element.getElementsByTagName("dav-bottom-sheet")[0]
+
+				openButton.onclick = () => {
 					bottomSheet.dismissable = false
 					bottomSheet.visible = true
 				}
@@ -69,18 +131,13 @@ module.exports = {
 				}
 			},
 			template: `
-				<dav-button id="open-button">
-					Open bottom sheet
-				</dav-button>
-
-				<dav-button id="open-non-dismissable-button" style="margin-top: 24px">
+				<dav-button>
 					Open non-dismissable bottom sheet
 				</dav-button>
 
 				<dav-bottom-sheet position="100">
 					<h1>Hello World</h1>
 					<dav-button
-						id="close-button"
 						style="margin-bottom: 24px"
 						size="small"
 					>Hide</dav-button>
