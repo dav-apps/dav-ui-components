f022d0007a1be90c00641f8da1d2bc8b242d7a3b
David <zurek.dav@gmail.com>
2023-08-27 19:23:56 +0200

    Add blur effect to ContextMenu component

--- src/context-menu/context-menu.styles.ts
+++ src/context-menu/context-menu.styles.ts
@@ -3,10 +3,11 @@ import { css } from "@lit/reactive-element"
 export const contextMenuStyles = css`
 	.container {
 		position: absolute;
-		background-color: var(--dav-color-surface);
+		background-color: rgb(var(--dav-color-surface-rgb), 0.75);
 		border: 1px solid rgb(var(--dav-color-outline-rgb), 0.12);
 		border-radius: 6px;
 		font-size: 16px;
 		box-shadow: 0px 8px 16px 0px rgb(var(--dav-color-shadow-rgb), 0.2);
+		backdrop-filter: blur(4px);
 	}
 `
