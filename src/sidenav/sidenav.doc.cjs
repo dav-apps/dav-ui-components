44861171084e78cba8bcf6f87c8791cf1d6246fb
David <zurek.dav@gmail.com>
2023-08-31 23:55:11 +0200

    Update Sidenav doc file with example for Sidenav in over mode

--- src/sidenav/sidenav.doc.cjs
+++ src/sidenav/sidenav.doc.cjs
@@ -2,22 +2,76 @@ module.exports = {
 	name: "Sidenav",
 	examples: [
 		{
-			title: "Inline Sidenav",
+			title: "Sidenav (inline)",
 			controller: function (element) {
 				let sidenavItems = element.getElementsByTagName("dav-sidenav-item")
 
-				for (let item of sidenavItems) {
-					item.addEventListener("click", () => {
-						for (let sidenavItem of sidenavItems) {
-							sidenavItem.active = false
+				for (let sidenavItem of sidenavItems) {
+					sidenavItem.addEventListener("click", () => {
+						for (let item of sidenavItems) {
+							item.active = false
 						}
 
-						item.active = true
+						sidenavItem.active = true
 					})
 				}
 			},
 			template: `
 				<dav-sidenav>
+					<dav-sidenav-item
+						value="Startseite"
+					></dav-sidenav-item>
+
+					<dav-sidenav-item
+						value="Abenteuer"
+						active
+					></dav-sidenav-item>
+
+					<dav-sidenav-item
+						value="Fiktion"
+					></dav-sidenav-item>
+
+					<dav-sidenav-item
+						value="Sachbücher"
+					></dav-sidenav-item>
+
+					<dav-sidenav-item
+						value="Poesie"
+					></dav-sidenav-item>
+				</dav-sidenav>
+			`
+		},
+		{
+			title: "Sidenav (over)",
+			controller: function (element) {
+				let sidenav = element.getElementsByTagName("dav-sidenav")[0]
+				let sidenavItems = element.getElementsByTagName("dav-sidenav-item")
+				let button = element.getElementsByTagName("dav-button")[0]
+
+				sidenav.addEventListener("dismiss", () => {
+					sidenav.open = false
+				})
+
+				for (let sidenavItem of sidenavItems) {
+					sidenavItem.addEventListener("click", () => {
+						for (let item of sidenavItems) {
+							item.active = false
+						}
+
+						sidenavItem.active = true
+					})
+				}
+
+				button.onclick = () => {
+					sidenav.open = true
+				}
+			},
+			template: `
+				<dav-button>
+					Open sidenav
+				</dav-button>
+
+				<dav-sidenav mode="over">
 					<dav-sidenav-item
 						value="Startseite"
 					></dav-sidenav-item>
