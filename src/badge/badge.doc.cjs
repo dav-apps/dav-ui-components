25babfbe5a24efab9080c2906724af10936dbe4c
David <zurek.dav@gmail.com>
2023-09-01 19:10:52 +0200

    Add doc file for Badge component

---
+++ src/badge/badge.doc.cjs
@@ -0,0 +1,53 @@
+module.exports = {
+	name: "Badge",
+	properties: [
+		{
+			name: "color",
+			type: "String",
+			values: `"primary", "secondary", "tertiary", "error"`
+		},
+		{
+			name: "tonal",
+			type: "Boolean",
+			values: "true / false"
+		}
+	],
+	examples: [
+		{
+			title: "Primary badge",
+			template: `
+				<div style="display: flex; gap: 20px">
+					<dav-badge>Hello World</dav-badge>
+					<dav-badge tonal>Hello World</dav-badge>
+				</div>
+			`
+		},
+		{
+			title: "Secondary badge",
+			template: `
+				<div style="display: flex; gap: 20px">
+					<dav-badge color="secondary">Hello World</dav-badge>
+					<dav-badge color="secondary" tonal>Hello World</dav-badge>
+				</div>
+			`
+		},
+		{
+			title: "Tertiary badge",
+			template: `
+				<div style="display: flex; gap: 20px">
+					<dav-badge color="tertiary">Hello World</dav-badge>
+					<dav-badge color="tertiary" tonal>Hello World</dav-badge>
+				</div>
+			`
+		},
+		{
+			title: "Error badge",
+			template: `
+				<div style="display: flex; gap: 20px">
+					<dav-badge color="error">Hello World</dav-badge>
+					<dav-badge color="error" tonal>Hello World</dav-badge>
+				</div>
+			`
+		}
+	]
+}
