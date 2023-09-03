61260c76b6a3f1cd74ac00171838a116feb11a07
David <zurek.dav@gmail.com>
2023-09-03 14:27:02 +0200

    Implement ProgressBar

--- src/progress-bar/progress-bar.styles.ts
+++ src/progress-bar/progress-bar.styles.ts
@@ -1,3 +1,21 @@
 import { css } from "@lit/reactive-element"
 
-export const progressBarStyles = css``
+export const progressBarStyles = css`
+	.container {
+		position: relative;
+	}
+
+	.track {
+		position: absolute;
+		width: 100%;
+		height: 4px;
+		background-color: var(--dav-color-surface-container-highest);
+	}
+
+	.indicator {
+		position: absolute;
+		height: 4px;
+		background-color: var(--dav-color-primary);
+		transition: 200ms;
+	}
+`
--- src/progress-bar/progress-bar.ts
+++ src/progress-bar/progress-bar.ts
@@ -1,5 +1,6 @@
 import { LitElement, html } from "lit"
-import { customElement } from "lit/decorators.js"
+import { customElement, property, state } from "lit/decorators.js"
+import { styleMap } from "lit/directives/style-map.js"
 import { globalStyles } from "../styles.js"
 import { progressBarStyles } from "./progress-bar.styles.js"
 
@@ -9,7 +10,24 @@ export const progressBarTagName = "dav-progress-bar"
 export class ProgressBar extends LitElement {
 	static styles = [globalStyles, progressBarStyles]
 
+	@state() private indicatorStyles = {
+		width: "0"
+	}
+
+	@property({ type: Number }) value: number = 0
+
 	render() {
-		return html``
+		this.indicatorStyles.width = `${this.value}%`
+
+		return html`
+			<div class="container">
+				<div class="track"></div>
+
+				<div
+					class="indicator"
+					style=${styleMap(this.indicatorStyles)}
+				></div>
+			</div>
+		`
 	}
 }
