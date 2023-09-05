b679862e814b376ce14b87819b397fc4110b045a
David <zurek.dav@gmail.com>
2023-09-05 20:56:55 +0200

    Implement ProfileCard layout & design

--- src/profile-card/profile-card.styles.ts
+++ src/profile-card/profile-card.styles.ts
@@ -1,3 +1,33 @@
 import { css } from "@lit/reactive-element"
 
-export const profileCardStyles = css``
+export const profileCardStyles = css`
+	.container {
+		display: inline-flex;
+		align-items: center;
+		padding: 8px;
+		background-color: var(--dav-color-surface);
+		color: var(--dav-color-on-surface);
+		border: 1px solid rgb(var(--dav-color-outline-rgb), 0.15);
+		border-radius: 12px;
+		box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075);
+		cursor: pointer;
+		transition: 200ms;
+	}
+
+	.container:hover,
+	.container:focus {
+		transform: translateY(-4px);
+		box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
+	}
+
+	dav-blurhash-image {
+		border-radius: 50%;
+	}
+
+	p {
+		margin: 0 12px 0 16px;
+		font-family: Roboto, Arial, sans-serif;
+		font-size: 16px;
+		font-weight: 400;
+	}
+`
--- src/profile-card/profile-card.ts
+++ src/profile-card/profile-card.ts
@@ -1,5 +1,11 @@
 import { LitElement, html } from "lit"
 import { customElement, property } from "lit/decorators.js"
+import { Settings } from "../types.js"
+import {
+	setThemeColorVariables,
+	subscribeSettingsChange,
+	unsubscribeSettingsChange
+} from "../utils.js"
 import { globalStyles } from "../styles.js"
 import { profileCardStyles } from "./profile-card.styles.js"
 
@@ -9,7 +15,40 @@ export const profileCardTagName = "dav-profile-card"
 export class ProfileCard extends LitElement {
 	static styles = [globalStyles, profileCardStyles]
 
+	@property() imageSrc: string = ""
+	@property() imageFallbackSrc: string = ""
+	@property() imageBlurhash: string = ""
+	@property() name: string = ""
+
+	connectedCallback() {
+		super.connectedCallback()
+		subscribeSettingsChange(this.settingsChange)
+	}
+
+	disconnectedCallback() {
+		super.disconnectedCallback()
+		unsubscribeSettingsChange(this.settingsChange)
+	}
+
+	settingsChange = (settings: Settings) => {
+		setThemeColorVariables(this.style, settings.theme)
+	}
+
 	render() {
-		return html``
+		return html`
+			<button class="container">
+				<dav-blurhash-image
+					src=${this.imageSrc}
+					fallbackSrc=${this.imageFallbackSrc}
+					imageBlurhash=${this.imageBlurhash}
+					title=${this.name}
+					alt=${this.name}
+					width="36"
+					height="36"
+				></dav-blurhash-image>
+
+				<p>${this.name}</p>
+			</button>
+		`
 	}
 }
