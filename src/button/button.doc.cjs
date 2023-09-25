caaa69783fd80bfc8bfce863f3d0b1b3c3bd799e
David <zurek.dav@gmail.com>
2023-09-25 18:55:37 +0200

    Update doc file with examples for left and right icons

--- src/button/button.doc.cjs
+++ src/button/button.doc.cjs
@@ -31,14 +31,14 @@ module.exports = {
 		{
 			title: "Primary button",
 			template: `
-				<div style="display: flex; gap: 30px">
+				<div style="display: flex; flex-wrap: wrap; gap: 30px">
 					<dav-button>Hello World</dav-button>
 					<dav-button tonal>Hello World</dav-button>
 					<dav-button outline>Hello World</dav-button>
 					<dav-button text>Hello World</dav-button>
 				</div>
 
-				<div style="display: flex; gap: 30px; margin-top: 30px">
+				<div style="display: flex; flex-wrap: wrap; gap: 30px; margin-top: 30px">
 					<dav-button size="small">Hello World</dav-button>
 					<dav-button size="small" tonal>Hello World</dav-button>
 					<dav-button size="small" outline>Hello World</dav-button>
@@ -49,14 +49,14 @@ module.exports = {
 		{
 			title: "Secondary button",
 			template: `
-				<div style="display: flex; gap: 30px">
+				<div style="display: flex; flex-wrap: wrap; gap: 30px">
 					<dav-button color="secondary">Hello World</dav-button>
 					<dav-button color="secondary" tonal>Hello World</dav-button>
 					<dav-button color="secondary" outline>Hello World</dav-button>
 					<dav-button color="secondary" text>Hello World</dav-button>
 				</div>
 
-				<div style="display: flex; gap: 30px; margin-top: 30px">
+				<div style="display: flex; flex-wrap: wrap; gap: 30px; margin-top: 30px">
 					<dav-button size="small" color="secondary">Hello World</dav-button>
 					<dav-button size="small" color="secondary" tonal>Hello World</dav-button>
 					<dav-button size="small" color="secondary" outline>Hello World</dav-button>
@@ -67,14 +67,14 @@ module.exports = {
 		{
 			title: "Tertiary tonal button",
 			template: `
-				<div style="display: flex; gap: 30px">
+				<div style="display: flex; flex-wrap: wrap; gap: 30px">
 					<dav-button color="tertiary">Hello World</dav-button>
 					<dav-button color="tertiary" tonal>Hello World</dav-button>
 					<dav-button color="tertiary" outline>Hello World</dav-button>
 					<dav-button color="tertiary" text>Hello World</dav-button>
 				</div>
 
-				<div style="display: flex; gap: 30px; margin-top: 30px">
+				<div style="display: flex; gap; flex-wrap: wrap: 30px; margin-top: 30px">
 					<dav-button size="small" color="tertiary">Hello World</dav-button>
 					<dav-button size="small" color="tertiary" tonal>Hello World</dav-button>
 					<dav-button size="small" color="tertiary" outline>Hello World</dav-button>
@@ -85,14 +85,14 @@ module.exports = {
 		{
 			title: "Error button",
 			template: `
-				<div style="display: flex; gap: 30px">
+				<div style="display: flex; flex-wrap: wrap; gap: 30px">
 					<dav-button color="error">Hello World</dav-button>
 					<dav-button color="error" tonal>Hello World</dav-button>
 					<dav-button color="error" outline>Hello World</dav-button>
 					<dav-button color="error" text>Hello World</dav-button>
 				</div>
 
-				<div style="display: flex; gap: 30px; margin-top: 30px">
+				<div style="display: flex; flex-wrap: wrap; gap: 30px; margin-top: 30px">
 					<dav-button size="small" color="error">Hello World</dav-button>
 					<dav-button size="small" color="error" tonal>Hello World</dav-button>
 					<dav-button size="small" color="error" outline>Hello World</dav-button>
@@ -103,18 +103,178 @@ module.exports = {
 		{
 			title: "Disabled button",
 			template: `
-				<div style="display: flex; gap: 30px">
+				<div style="display: flex; flex-wrap: wrap; gap: 30px">
 					<dav-button disabled>Hello World</dav-button>
 					<dav-button tonal disabled>Hello World</dav-button>
 					<dav-button outline disabled>Hello World</dav-button>
 					<dav-button text disabled>Hello World</dav-button>
 				</div>
 
-				<div style="display: flex; gap: 30px; margin-top: 30px">
+				<div style="display: flex; flex-wrap: wrap; gap: 30px; margin-top: 30px">
 					<dav-button size="small" disabled>Hello World</dav-button>
 					<dav-button size="small" tonal disabled>Hello World</dav-button>
 					<dav-button size="small" outline disabled>Hello World</dav-button>
 					<dav-button size="small" text disabled>Hello World</dav-button>
+				</div>
+			`
+		},
+		{
+			title: "Button with icon left",
+			template: `
+				<div style="display: flex; flex-wrap: wrap; gap: 30px">
+					<dav-button>
+						<i
+							slot="icon-left"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+
+						Hello World
+					</dav-button>
+
+					<dav-button tonal>
+						<i
+							slot="icon-left"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+					
+						Hello World
+					</dav-button>
+
+					<dav-button outline>
+						<i
+							slot="icon-left"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+
+						Hello World
+					</dav-button>
+
+					<dav-button text>
+						<i
+							slot="icon-left"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+
+						Hello World
+					</dav-button>
+				</div>
+
+				<div style="display: flex; flex-wrap: wrap; gap: 30px; margin-top: 30px">
+					<dav-button size="small">
+						<i
+							slot="icon-left"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+
+						Hello World
+					</dav-button>
+
+					<dav-button size="small" tonal>
+						<i
+							slot="icon-left"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+
+						Hello World
+					</dav-button>
+
+					<dav-button size="small" outline>
+						<i
+							slot="icon-left"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+
+						Hello World
+					</dav-button>
+
+					<dav-button size="small" text>
+						<i
+							slot="icon-left"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+
+						Hello World
+					</dav-button>
+				</div>
+			`
+		},
+		{
+			title: "Button with icon right",
+			template: `
+				<div style="display: flex; flex-wrap: wrap; gap: 30px">
+					<dav-button>
+						<i
+							slot="icon-right"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+
+						Hello World
+					</dav-button>
+
+					<dav-button tonal>
+						<i
+							slot="icon-right"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+					
+						Hello World
+					</dav-button>
+
+					<dav-button outline>
+						<i
+							slot="icon-right"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+
+						Hello World
+					</dav-button>
+
+					<dav-button text>
+						<i
+							slot="icon-right"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+
+						Hello World
+					</dav-button>
+				</div>
+
+				<div style="display: flex; flex-wrap: wrap; gap: 30px; margin-top: 30px">
+					<dav-button size="small">
+						<i
+							slot="icon-right"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+
+						Hello World
+					</dav-button>
+
+					<dav-button size="small" tonal>
+						<i
+							slot="icon-right"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+
+						Hello World
+					</dav-button>
+
+					<dav-button size="small" outline>
+						<i
+							slot="icon-right"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+
+						Hello World
+					</dav-button>
+
+					<dav-button size="small" text>
+						<i
+							slot="icon-right"
+							class="fa-solid fa-magnifying-glass"
+						></i>
+
+						Hello World
+					</dav-button>
 				</div>
 			`
 		}
