35186fb17db23d46c66976e56eef46cc98d3e953
David <zurek.dav@gmail.com>
2023-12-29 18:31:35 +0100

    Update doc file for Search to show simple search result data in a List

--- src/search/search.doc.cjs
+++ src/search/search.doc.cjs
@@ -3,8 +3,24 @@ module.exports = {
 	examples: [{
 		title: "Default Search",
 		controller: function (element) {
+			const sampleResultData = [
+				{
+					headline: "The Portrait of a Lady",
+					subhead: "Henry James",
+					imageSrc: "https://dav-backend.fra1.cdn.digitaloceanspaces.com/db95a259-6da1-4986-82f8-d1af6fc6d4f6",
+					imageAspectRatio: "1:1.5"
+				},
+				{
+					headline: "Dombey and Son",
+					subhead: "Charles Dickens",
+					imageSrc: "https://dav-backend.fra1.cdn.digitaloceanspaces.com/6366cd03-8cd7-4904-974c-e65ee02800e6",
+					imageAspectRatio: "1:1.5"
+				}
+			]
+
 			let openButton = element.getElementsByTagName("dav-button")[0]
 			let search = element.getElementsByTagName("dav-search")[0]
+			let list = element.getElementsByTagName("dav-list")[0]
 
 			openButton.onclick = () => {
 				search.visible = true
@@ -19,6 +35,20 @@ module.exports = {
 
 				setTimeout(() => {
 					search.loading = false
+
+					if (list.children.length == 0) {
+						// Show the sample data
+						for (let item of sampleResultData) {
+							let listItem = document.createElement("dav-list-item")
+
+							listItem.headline = item.headline
+							listItem.subhead = item.subhead
+							listItem.imageSrc = item.imageSrc
+							listItem.imageAspectRatio = item.imageAspectRatio
+
+							list.append(listItem)
+						}
+					}
 				}, 2000)
 			})
 		},
@@ -27,9 +57,9 @@ module.exports = {
 				Open search
 			</dav-button>
 
-			<dav-search
-				placeholder="Search for..."
-			></dav-search>
+			<dav-search placeholder="Search for...">
+				<dav-list></dav-list>
+			</dav-search>
 		`
 	}]
 }
