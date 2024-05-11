a7387e3ea3e9685832e4af7fe8f23c75c7ee8e0a
David <zurek.dav@gmail.com>
2024-05-12 00:30:21 +0200

    Add border and shadow to calendar container

--- src/calendar/calendar.styles.ts
+++ src/calendar/calendar.styles.ts
@@ -4,12 +4,17 @@ export const calendarStyles = css`
 	.container {
 		display: inline-block;
 		background-color: var(--dav-color-surface);
+		border: 1px solid rgb(var(--dav-color-outline-rgb), 0.15);
+		border-radius: 12px;
+		box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075);
+		padding: 6px 12px 12px;
 	}
 
 	.top-container {
 		display: flex;
 		align-items: center;
 		justify-content: space-evenly;
+		margin-bottom: 6px;
 	}
 
 	.bottom-container {
