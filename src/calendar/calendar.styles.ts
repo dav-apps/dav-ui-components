6bfbcd1423e84d3181d7a8d6f94662276790f860
David <zurek.dav@gmail.com>
2024-05-12 00:15:59 +0200

    Implement basic calendar view

--- src/calendar/calendar.styles.ts
+++ src/calendar/calendar.styles.ts
@@ -1,3 +1,33 @@
 import { css } from "@lit/reactive-element"
 
-export const calendarStyles = css``
+export const calendarStyles = css`
+	.container {
+		display: inline-block;
+		background-color: var(--dav-color-surface);
+	}
+
+	.top-container {
+		display: flex;
+		align-items: center;
+		justify-content: space-evenly;
+	}
+
+	.arrow-right svg {
+		transform: rotate(180deg);
+	}
+
+	.weekdays-container {
+		display: flex;
+		gap: 8px;
+	}
+
+	.month-container {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.day-container {
+		display: flex;
+		justify-content: center;
+	}
+`
--- src/calendar/calendar.ts
+++ src/calendar/calendar.ts
@@ -1,5 +1,7 @@
 import { LitElement, html } from "lit"
 import { customElement, property, state } from "lit/decorators.js"
+import { DateTime } from "luxon"
+import { arrowLeftLightSvg } from "../../assets/svg/arrow-left-light.js"
 import { globalStyles } from "../styles.js"
 import { calendarStyles } from "./calendar.styles.js"
 
@@ -9,9 +11,108 @@ export const calendarTagName = "dav-calendar"
 export class Calendar extends LitElement {
 	static styles = [globalStyles, calendarStyles]
 
+	getWeekdays() {
+		let currentWeekDay = DateTime.now().startOf("week")
+		let weekDays: string[] = []
+
+		for (let i = 0; i < 7; i++) {
+			weekDays.push(currentWeekDay.weekdayShort)
+			currentWeekDay = currentWeekDay.plus({ days: 1 })
+		}
+
+		return weekDays
+	}
+
+	getWeekdaysHtml() {
+		let weekdayHtml = []
+
+		for (let day of this.getWeekdays()) {
+			weekdayHtml.push(
+				html`
+					<div>${day}</div>
+				`
+			)
+		}
+
+		return weekdayHtml
+	}
+
+	getDays() {
+		let weeks: number[][] = []
+
+		let currentMonth = DateTime.now().month
+		let currentDate = DateTime.now()
+			.startOf("month")
+			.startOf("week")
+
+		while (currentDate.month <= currentMonth) {
+			let row: number[] = []
+
+			for (let i = 0; i < 7; i++) {
+				row.push(currentDate.day)
+				currentDate = currentDate.plus({ days: 1 })
+			}
+
+			weeks.push(row)
+		}
+
+		return weeks
+	}
+
+	getDaysHtml() {
+		let weeks = this.getDays()
+		let weeksHtml = []
+
+		for (let week of weeks) {
+			let weekHtml = []
+
+			for (let day of week) {
+				weekHtml.push(
+					html`
+						<div class="day-container">${day}</div>
+					`
+				)
+			}
+
+			weeksHtml.push(
+				html`
+					<div>
+						${weekHtml}
+					</div>
+				`
+			)
+		}
+
+		return weeksHtml
+	}
+
 	render() {
 		return html`
-			<p>Hello World</p>
+			<div class="container">
+				<div class="top-container">
+					<div class="arrow-left">
+						<dav-icon-button size="small">
+							${arrowLeftLightSvg}
+						</dav-icon-button>
+					</div>
+
+					<div class="date-container">May 2024</div>
+
+					<div class="arrow-right">
+						<dav-icon-button size="small">
+							${arrowLeftLightSvg}
+						</dav-icon-button>
+					</div>
+				</div>
+				<div class="bottom-container">
+					<div class="weekdays-container">
+						${this.getWeekdaysHtml()}
+					</div>
+					<div class="month-container">
+						${this.getDaysHtml()}
+					</div>
+				</div>
+			</div>
 		`
 	}
 }
