a4d329cd48caade4aa1b212d82c62c5cbfaaf8ff
David <zurek.dav@gmail.com>
2024-05-13 23:25:23 +0200

    Add input fields for hour and minute inputs

--- src/time-picker/time-picker.styles.ts
+++ src/time-picker/time-picker.styles.ts
@@ -1,3 +1,14 @@
 import { css } from "@lit/reactive-element"
 
-export const timePickerStyles = css``
+export const timePickerStyles = css`
+	.container {
+		display: inline-flex;
+		align-items: baseline;
+		gap: 8px;
+	}
+
+	.hour-input-container,
+	.minute-input-container {
+		width: 38px;
+	}
+`
--- src/time-picker/time-picker.ts
+++ src/time-picker/time-picker.ts
@@ -1,5 +1,5 @@
 import { LitElement, html } from "lit"
-import { customElement } from "lit/decorators.js"
+import { customElement, state } from "lit/decorators.js"
 import { globalStyles } from "../styles.js"
 import { timePickerStyles } from "./time-picker.styles.js"
 
@@ -9,9 +9,56 @@ export const timePickerTagName = "dav-time-picker"
 export class TimePicker extends LitElement {
 	static styles = [globalStyles, timePickerStyles]
 
+	@state() private hour: string = "14"
+	@state() private minute: string = "00"
+
+	hourInputChange(event: CustomEvent) {
+		let hour = event.detail.value
+		let numValue = Number(hour)
+
+		if (numValue < 10 && numValue >= 0) {
+			this.hour = `0${numValue}`
+		} else {
+			this.hour = hour
+		}
+	}
+
+	minuteInputChange(event: CustomEvent) {
+		let minute = event.detail.value
+		let numValue = Number(minute)
+
+		if (numValue < 10 && numValue >= 0) {
+			this.minute = `0${numValue}`
+		} else {
+			this.minute = minute
+		}
+	}
+
 	render() {
 		return html`
-			<p>Hello World</p>
+			<div class="container">
+				<div class="hour-input-container">
+					<dav-textfield
+						value=${this.hour}
+						type="number"
+						min="0"
+						max="23"
+						noArrows
+						@change=${this.hourInputChange}
+					></dav-textfield>
+				</div>
+				:
+				<div class="minute-input-container">
+					<dav-textfield
+						value=${this.minute}
+						type="number"
+						min="0"
+						max="59"
+						noArrows
+						@change=${this.minuteInputChange}
+					></dav-textfield>
+				</div>
+			</div>
 		`
 	}
 }
