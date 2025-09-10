b25b2237fd952d219559256168e8aaf6f4707339
David <zurek.dav@gmail.com>
2025-09-10 22:10:30 +0200

    Add example for customized skeleton in doc file

--- src/skeleton/skeleton.doc.cjs
+++ src/skeleton/skeleton.doc.cjs
@@ -1,10 +1,17 @@
 module.exports = {
 	name: "Skeleton",
-	properties: [],
 	examples: [
 		{
 			title: "Default Skeleton",
 			template: `<dav-skeleton></dav-skeleton>`
+		},
+		{
+			title: "Customized Skeleton",
+			template: `
+				<dav-skeleton
+					style="width: 130px; height: 40px; border-radius: 20px;"
+				></dav-skeleton>
+			`
 		}
 	]
 }
